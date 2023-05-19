5c44f96097f3fec28f73729d1a38a5b41a87e9fe
lenagend <lenagend@naver.com>
2023-05-19 15:26:18 +0900

    [2023-05-19] 조회수 요청 패치

--- src/component/post/useIncrementViewCount.js
+++ src/component/post/useIncrementViewCount.js
@@ -6,21 +6,19 @@ import Cookies from 'js-cookie';
 const useIncrementViewCount = (postId) => {
     useEffect(() => {
         const incrementViewCount = async () => {
-            // 쿠키에서 조회수를 가져옵니다.
-            const viewCount = Cookies.get(`viewCount-${postId}`);
+            const uniqueId = localStorage.getItem('uniqueId');
 
-            // 만약 쿠키가 없으면 (즉, 사용자가 이 게시물을 처음 본 경우), 조회수를 증가시키고 쿠키를 설정합니다.
-            if (!viewCount) {
-                try {
-                    await axios.post(`${API_BASE_URL}/api/posts/${postId}/view`);
-                    // 조회수를 증가시킨 후, 쿠키를 설정하여 사용자가 이 게시물을 이미 본 것을 표시합니다.
-                    // 이 쿠키는 24시간 후에 만료됩니다.
-                    Cookies.set(`viewCount-${postId}`, 'viewed', { expires: 1 });
-                } catch (error) {
-                    console.error("Error incrementing view count", error);
-                }
+            try {
+                await axios.post(`${API_BASE_URL}/api/posts/${postId}/view`, null, {
+                    params: {
+                        uniqueId: uniqueId
+                    }
+                });
+            } catch (error) {
+                console.error("Error incrementing view count", error);
             }
         };
+
         incrementViewCount();
     }, [postId]);
 };
